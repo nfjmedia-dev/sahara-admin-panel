b2f1e69dc31e1b2ad3ef57816181672674641b77
Hassan Nawaz <hassannawaz4399@gmail.com>
2024-09-23 17:45:36 +0500

    headers

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,8 +2,17 @@
 import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL;
-
+const headers = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'cache-control': 'no-cache, no-store', 
+    'content-type': 'application/x-www-form-urlencoded',
+    'pragma': 'no-cache',
+    'referrer-policy': 'strict-origin',
+    'x-custom-name': 'Borgun hf.',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'
+  };
 export const apiService = {
+    
     get: async (endpoint: string) => {
         const response = await axios.get(`${API_URL}/${endpoint}`);
         return response.data;
@@ -16,11 +25,12 @@ export const apiService = {
     //     console.log(response)
     //     return response.data;
     // },
+    
     create: async (endpoint: string, data: any) => {
         console.log("endpoint",endpoint)
         console.log("data",data)
         console.log("API_URL",`${endpoint}`)
-        const response = await axios.post(`${endpoint}`, data);
+        const response = await axios.post(`${endpoint}`, data,{headers});
         console.log(response)
         return response.data;
     },
