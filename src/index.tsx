59d43dc3f6f99640b034b8d9c3be5f7a2946b833
Hassan Nawaz <hassannawaz4399@gmail.com>
2024-12-15 06:21:53 +0500

    admin panel updated

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-// src/index.tsx or src/App.tsx
-import 'bootstrap-icons/font/bootstrap-icons.css'; // For Bootstrap Icons
-// or
-import '@fortawesome/fontawesome-free/css/all.min.css'; // For Font Awesome
-
+import 'bootstrap-icons/font/bootstrap-icons.css'; // Or Font Awesome, choose one
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
